f9e8efae36af15d53a6f46a25382fcc3c375bc13
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-03-28 00:10:08 -0700

    Comment section props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45"
+        content="Informative. Thanks."
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="John"
+        timeAgo="Today at 2:25"
+        content="Great!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Rick"
+        timeAgo="Today at 8:00"
+        content="Nice blog post."
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
